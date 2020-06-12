d20c62c18bc1659ee1094230544013e19971d35c
Kauser01 <kauserfatima01@gmail.com>
2020-06-12 11:26:47 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.get('/data', function (req, res) {
+  console.log("In data");
+  setTimeout(function(){
+    console.log("before sending the response");
+    res.setHeader("Access-Control-Request-Methods", "GET");
+    res.send({data:'Response from server.'});
+  },80000);
+});
+
+app.listen(3000, function () {
+  console.log('info', 'Test app listening on port 3000!');
+});
+
+
+app.set('views', __dirname + '/client/');
+
+app.use(express.static(path.join(__dirname, '/client')));
---
+++ client/app.js
@@ -0,0 +1,25 @@
+angular
+    .module("TestApp", [
+        'ui.router',
+        'ngResource'
+    ]).controller('MainCtrl', ['$scope', '$q', '$http', function ($scope, $q, $http) {
+        $scope.responseData = " ";
+        $scope.testFunction = function (uri, verb, params, data) {
+            $scope.responseData = " ";
+            var req = {
+                method: verb,
+                url: uri,
+                timeout:300000
+            }
+            console.log("Request made for:"+req.url);
+            $("#Loading").text("Waiting for response...");
+            $http(req).success(function (data, status) {
+                $("#Loading").text("Got the response!");
+                console.log("Got response for "+req.url+":"+data);
+                $scope.responseData = data;
+            }).error(function (data, status, headers, config) {
+                $scope.responseData = "In Error callback";
+               console.log(data);
+            });
+        };
+    }]);
